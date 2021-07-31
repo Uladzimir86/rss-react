b5e4812191583c42199e0aa242ae97177855f186
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2021-07-31 17:04:32 +0300

    set webpack configuration

---
+++ react-components/src/index.tsx
@@ -0,0 +1,7 @@
+import './style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// let document: Document;
+const elem = <h1>Привет, мир!</h1>;
+ReactDOM.render(elem, document.getElementById('root'));
