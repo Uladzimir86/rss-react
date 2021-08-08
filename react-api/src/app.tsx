c41f5c4a4b699fda59eb582796926e8cda5534ba
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2021-08-08 17:37:57 +0300

    init: start react-api

---
+++ react-api/src/app.tsx
@@ -0,0 +1,24 @@
+import React, { ReactElement } from 'react';
+
+
+// const SearchBar = () => (
+//     <div className="searchBar">
+//       <input type="text" className="searchBar__input" placeholder="Search" />
+//     </div>
+//   )
+// const Main = () => {
+//   const cardsArray = cards[1].map((item) => (
+//     <Card key={item.word} image={item.image} word={item.word} translation={item.translation}/>
+//     ))
+//   return (
+//     <div className="main">
+//       { cardsArray }
+//     </div>
+//   )
+// }
+const App = (): ReactElement => (
+    <div>
+      w
+    </div>
+  )
+export default App;
