9e52887044142739a6e790c5f0befaaead65853b
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2021-09-08 13:57:51 +0300

    fix: set prittier

--- react-components/src/app.tsx
+++ react-components/src/app.tsx
@@ -3,24 +3,25 @@ import cards from './cards';
 import Card from './card';
 
 const SearchBar = () => (
-    <div className="searchBar">
-      <input type="text" className="searchBar__input" placeholder="Search" />
-    </div>
-  )
+  <div className="searchBar">
+    <input type="text" className="searchBar__input" placeholder="Search" />
+  </div>
+);
 const Main = () => {
   const cardsArray = cards[1].map((item) => (
-    <Card key={item.word} image={item.image} word={item.word} translation={item.translation}/>
-    ))
-  return (
-    <div className="main">
-      { cardsArray }
-    </div>
-  )
-}
+    <Card
+      key={item.word}
+      image={item.image}
+      word={item.word}
+      translation={item.translation}
+    />
+  ));
+  return <div className="main">{cardsArray}</div>;
+};
 const App = (): ReactElement => (
-    <div>
-      <SearchBar />
-      <Main /> 
-    </div>
-  )
+  <div>
+    <SearchBar />
+    <Main />
+  </div>
+);
 export default App;
--- react-components/src/card.tsx
+++ react-components/src/card.tsx
@@ -1,13 +1,23 @@
 import React, { ReactElement } from 'react';
 
-const Card = ({key, image, word, translation}: {key: string, image: string, word: string, translation: string}): ReactElement => (
-    <div className="card" key={key}>
-      <div className="card__foto-container">
-        <img src={`./${image}`} alt={word}  className="card__foto"/>
-      </div>
-      <div className="card__word_en">{word}</div>
-      <div className="card__word_ru">{translation}</div>
+const Card = ({
+  key,
+  image,
+  word,
+  translation
+}: {
+  key: string;
+  image: string;
+  word: string;
+  translation: string;
+}): ReactElement => (
+  <div className="card" key={key}>
+    <div className="card__foto-container">
+      <img src={`./${image}`} alt={word} className="card__foto" />
     </div>
-  )
+    <div className="card__word_en">{word}</div>
+    <div className="card__word_ru">{translation}</div>
+  </div>
+);
 
 export default Card;
